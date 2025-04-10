e4068806c909957265bf4a3b90ff8c93112b2478
Mohammed Maqsood <mohammedmaqsood@Mohammeds-MBP.localdomain>
2025-04-10 16:08:19 +0100

    Added more features

--- Script.js
+++ Script.js
@@ -1,3 +1,9 @@
 function ToggleDarkMode(){
     document.body.classList.toggle("DarkMode")
 }
+function OpenSideBar(){
+    document.getElementById("sideNav").style.height="250px"
+}
+function CloseBar(){
+    document.getElementById("sideNav").style.height="0"
+}
