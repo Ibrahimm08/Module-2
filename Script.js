58a58287db8f0b08e13e8d1eb785fa02781a14ea
Mohammed Maqsood <mohammedmaqsood@Mohammeds-MBP.localdomain>
2025-04-11 10:14:58 +0100

    Added sidebar functionality for smalled displays

--- Script.js
+++ Script.js
@@ -1,9 +1,40 @@
+
+
+let width = window.innerWidth
+
+
+
+
 function ToggleDarkMode(){
     document.body.classList.toggle("DarkMode")
 }
+
+
+//--------------------------Side Bar----------------------------
+
+// Side Bar not appearing on resize after closing fix
+function resizeEvent(){
+    if (width > 1024){
+        document.getElementById("sideNav").style.height="100%"
+        document.getElementById("sideNav").style.position="static"
+    }
+    // If we don't do this the buttons won't do anything :(
+    else if (width <= 1024){
+        document.getElementById("sideNav").style.height="0"
+        document.getElementById("sideNav").style.position="fixed"
+    }
+}
+
 function OpenSideBar(){
-    document.getElementById("sideNav").style.height="250px"
+    if (width <= 1024){
+    document.getElementById("sideNav").style.height="45%"
+    document.getElementById("sideNav").style.paddingBottom="10px"
+    }
 }
 function CloseBar(){
+    if (width <= 1024){
     document.getElementById("sideNav").style.height="0"
+    document.getElementById("sideNav").style.paddingBottom="0"
+    }
 }
+
